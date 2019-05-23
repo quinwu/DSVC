839304cf57fc3e6b31632e651cf5fabd54f406ef
quinwu <wukuan1995@outlook.com>
2019-05-23 14:27:34 +0800

    modified cPickle -> pickle

--- assignment1/classwork/DSVC/data_utils.py
+++ assignment1/classwork/DSVC/data_utils.py
@@ -1,4 +1,4 @@
-import cPickle as pickle
+import pickle as pickle
 import numpy as np
 import os
 from scipy.misc import imread
@@ -71,7 +71,7 @@ def load_tiny_imagenet(path, dtype=np.float32):
     y_train = []
     for i, wnid in enumerate(wnids):
         if (i + 1) % 20 == 0:
-            print 'loading training data for synset %d / %d' % (i + 1, len(wnids))
+            print ('loading training data for synset %d / %d' % (i + 1, len(wnids)))
         # To figure out the filenames we need to open the boxes file
         boxes_file = os.path.join(path, 'train', wnid, '%s_boxes.txt' % wnid)
         with open(boxes_file, 'r') as f:
